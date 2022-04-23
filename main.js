ec5937367f545cf45546510b5b205c46750dfd7e
Jeremiah King <usaxray25@gmail.com>
2022-04-23 09:21:10 -0500

    fixed placeholder and other code

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 console.log("Connected!")
 // YOU WILL MODIFY THIS FUNCTION TO GET THE PROGRAM TO WORK
+
 const wordCounter = (value) => {
   // Type into the input and press submit, you will see the value you entered in your console
   console.log(value);
 
   if (value) {
     // COMPLETE THE LOGIC 
-    wordCount.innerHTML = `Word Count: 0`; 
+    wordCount.innerHTML = `Word Count: ${value.split(" ").length}`; //value.split(separator, limit)//
   } else {
     // if the value is empty, set the error message value to "Please input text"
-    error.innerHTML = ""; // UPDATE THIS
+    error.innerHTML = "Please input text"; // UPDATE THIS
   }
 }
 
 // OPTIONAL CHALLENGE
 const toggleMode = (btnText) => {
-  // complete the function
+
 }
 
 // ************************************************ //
